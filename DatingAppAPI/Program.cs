8884a68f8019cebe762cfae4a095d68f63185e39
raogpip <vitalik.fedorenko.2018@gmail.com>
2023-01-02 22:33:54 +0100

    API response + angular

--- DatingAppAPI/Program.cs
+++ DatingAppAPI/Program.cs
@@ -5,9 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options =>
+{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
@@ -15,6 +17,8 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
+app.UseCors(corsBuilder => corsBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
